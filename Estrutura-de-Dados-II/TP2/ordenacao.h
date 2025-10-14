0994d3b5d72c122bb42ebb6a524bee00a43175c0
marinacanal <eromakcanal@gmail.com>
2025-10-14 14:34:11 -0300

    renomeado ED-II/TP1 e add estrutura inicial TP2

---
+++ Estrutura-de-Dados-II/TP2/ordenacao.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int* clonaLista(int *lista, int tamanhoLista);
+void bubbleSort(int *lista, int tamanhoLista);
+void selectionSort(int *lista, int tamanhoLista);
+void insertionSort(int *lista, int tamanhoLista);
+void quickSort(int *lista, int tamanhoLista);
